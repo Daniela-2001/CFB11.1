8162bcee71766c5c6621b15f516775c81947c793
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-19 20:14:28 +0000

    CFB11.1

---
+++ aula11.1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace aula11._1
+{
+    internal class Program
+    {
+        static void Main(){
+
+            float n1 = 10.5f;
+            int n2 = (int)n1;//type cast
+
+            Console.WriteLine("n2");
+        }
+    }
+}
